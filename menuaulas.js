65fcc5099e95c3972233601f26474f491f03abea
Leonan Brayan <leonanbryan@gmail.com>
2021-06-08 22:42:43 -0300

    criando arquivos javascripit para usar em varias paginas

---
+++ menuaulas.js
@@ -0,0 +1,7 @@
+function abrirMenu() {
+    document.getElementById('open').style.height = 'auto';
+    document.getElementById('menu').style.width = '100%';
+  }
+  function fecharMenu() {
+    document.getElementById('open').style.height = '42px';
+  }
