f149c73a8438a2f1bc345b33a2c4bba7b2c57f4e
Leonan Brayan <leonanbryan@gmail.com>
2021-06-18 22:25:18 -0300

    criando pasta javascript

---
+++ javascript/menu.js
@@ -0,0 +1,12 @@
+function abrirMenu() {
+    document.getElementById('open').style.height = 'auto';
+  }
+  function fecharMenu() {
+    document.getElementById('open').style.height = '0px';
+  }
+  function abrirAulas() {
+    document.getElementById('openaulas').style.height = '240px';
+  }
+  function fecharAulas() {
+    document.getElementById('openaulas').style.height = '0px';
+  }
--- menu.js
+++
@@ -1,7 +0,0 @@
-function abrirMenu() {
-    document.getElementById('open').style.height = 'auto';
-    document.getElementById('menu').style.width = '100%';
-  }
-  function fecharMenu() {
-    document.getElementById('open').style.height = '0px';
-  }
