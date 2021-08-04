80c0dc4a644925915579887a3aa3ca454e0b9e39
LeonanBrayan <79603372+LeonanBrayan@users.noreply.github.com>
2021-08-04 20:07:15 -0300

    ajustes

--- conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $servidor = "localhost";
-    $usuario = "root";
-    $dbname = "python";
-    //criar a conexao
-    $conn = mysql_connect ($servidor, $usuario, $dbname);
-?>
--- formulario.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include_once("conexao.php");
-    $vnome=$_POST ["name"];
-    $vemail=$_POST ["email"];
-
-    $result_tabalunos = "INSERT INTO alunos (nome, email) VALUES ('$vnome','$vemail')";
-    $resultado_tabalunos = mysqli_query($conn, $result_tabalunos);
-?>
